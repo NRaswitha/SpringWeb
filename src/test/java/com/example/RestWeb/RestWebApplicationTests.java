95b4b730bf16b3fdf25fdcd8d3dfd9e380ac0ff1
Raswitha Narla <raswitha.narla66@gmail.com>
2021-05-28 13:43:56 -0400

    JavaSpring

---
+++ src/test/java/com/example/RestWeb/RestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
