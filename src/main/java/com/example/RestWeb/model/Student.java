03f5915044bbfdcab481646c386e53f34da378e6
Raswitha Narla <raswitha.narla66@gmail.com>
2021-06-02 13:02:48 -0400

    imported student details

--- src/main/java/com/example/RestWeb/model/Student.java
+++ src/main/java/com/example/RestWeb/model/Student.java
@@ -1,4 +1,47 @@
 package com.example.RestWeb.model;
 
-public class Student {
+public class Student
+{
+    private String name;
+    private String course;
+    private String description;
+
+    public Student(String name, String course, String description) {
+        this.name = name;
+        this.course = course;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", course='" + course + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
